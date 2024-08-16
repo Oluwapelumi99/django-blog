c87c08fc0dfedba5acb97223d3da887f03fd51d9
Oluwapelumi99 <156908824+Oluwapelumi99@users.noreply.github.com>
2024-08-16 11:10:48 +0100

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -14,7 +14,7 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
-    feild_1 = models.CharField()
+    
     class Meta:
             ordering = ["-created_on"]
 
